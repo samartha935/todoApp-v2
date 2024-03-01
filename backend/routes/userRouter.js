4777dcb3a7b00c6bd96de788747910bbfc737662
samartha935 <147503219+samartha935@users.noreply.github.com>
2024-03-01 11:55:12 +0530

    added route to delete an account

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -11,7 +11,6 @@ userRouter.post("/signup", async (req, res) => {
   try {
     const payload = req.body;
     const parsedPayload = signupSchema.safeParse(payload);
-
     if (!parsedPayload.success) {
       return res.status(411).json({
         msg: "You have entered wrong inputs.",
@@ -21,7 +20,6 @@ userRouter.post("/signup", async (req, res) => {
     const user = await User.findOne({
       $or: [{ username: payload.username }, { email: payload.email }],
     });
-
     if (user) {
       return res.json({
         msg: "This user already exists.",
@@ -32,7 +30,6 @@ userRouter.post("/signup", async (req, res) => {
     payload.password = await bcrypt.hash(payload.password, salt);
 
     const accountCreate = await User.create(payload);
-
     if (!accountCreate) {
       return res.json({
         msg: "There was error in creating account.",
@@ -50,37 +47,54 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.get("/signin", async (req, res) => {
-  const payload = req.body;
-  const parsedPayload = signinSchema.safeParse(payload);
+userRouter.delete("/signup", async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
 
-  if (!parsedPayload.success) {
+  const userInfo = jwt.decode(token, JWT_SECRATE_KEY).user;
+
+  const deleteAccount = await User.deleteOne(userInfo);
+  if (!deleteAccount.acknowledged || !deleteAccount.deletedCount) {
     return res.json({
-      msg: "You have entered wrong inputs.",
+      msg: "Failed to delete account.",
     });
   }
-
-  const user = await User.findOne({ username: payload.username });
-
-  if (!user) {
-    return res.json({
-      msg: "There is no account with the entered username.",
-    });
-  }
-
-
-  const result = await bcrypt.compare(payload.password, user.password);
-
-  if (!result) {
-    return res.json({
-      msg: "You have entered wrong password.",
-    });
-  }
-
-  const token = jwt.sign({ user }, JWT_SECRATE_KEY);
 
   return res.json({
-    msg: "You have logged in to the account.",
-    token: token,
+    msg: "Account deleted sucessfully.",
   });
+});
+
+userRouter.get("/signin", async (req, res) => {
+  try {
+    const payload = req.body;
+    const parsedPayload = signinSchema.safeParse(payload);
+    if (!parsedPayload.success) {
+      return res.json({
+        msg: "You have entered wrong inputs.",
+      });
+    }
+
+    const user = await User.findOne({ username: payload.username });
+    if (!user) {
+      return res.json({
+        msg: "There is no account with the entered username.",
+      });
+    }
+
+    const result = await bcrypt.compare(payload.password, user.password);
+    if (!result) {
+      return res.json({
+        msg: "You have entered wrong password.",
+      });
+    }
+
+    const token = jwt.sign({ user }, JWT_SECRATE_KEY);
+
+    return res.json({
+      msg: "You have logged in to the account.",
+      token: token,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });
